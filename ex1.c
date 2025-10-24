22099853dfb0b16fed7f72434c017061e35ba538
Emre <65902526+EmreDr@users.noreply.github.com>
2025-10-24 16:05:15 +0300

    number guess game (M.Emre Dere)

--- ex1.c
+++ ex1.c
@@ -1,1 +1,24 @@
 
+
+#include <stdio.h>
+// Guess number game -Made by Muhammed Emre Dere-
+int main() {
+
+    int correctNumber = 3;
+    int guess;
+  
+    printf("Find secret number between 1-5\n");
+    do {
+
+        printf("Enter your guess(and please just use number I did not add anti infinityloop system ): ");
+        scanf("%d", &guess);
+
+        if (guess != correctNumber) {
+            printf("Wrong guess. Try again\n");
+        }
+    } while (guess != correctNumber);
+    printf("Congrats you find the number.\n");
+    return 0;
+}
+
+
