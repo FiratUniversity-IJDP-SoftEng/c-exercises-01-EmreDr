ca13d5b2b2ddfb7cfa24f629d4b0045881e86bf0
Emre <65902526+EmreDr@users.noreply.github.com>
2025-10-24 18:08:33 +0300

    Add sayHello function to greet users by name

--- ex5.c
+++ ex5.c
@@ -1,1 +1,14 @@
+//Write a function called sayHello that takes a person's name and prints "Hello" with their name.
+//Made by Muhammed Emre Dere
+#include <stdio.h>
+void sayHello(const char* name) {
 
+    printf("Hello, %s!\n", name);
+}
+int main() {
+    char usersName[50];
+
+    printf("Please enter your name: ");
+    scanf("%49s", usersName);
+    sayHello(usersName);
+}
