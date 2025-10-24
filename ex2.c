776904e257d9566a146a0066b68b5430da4bfe3c
Emre <65902526+EmreDr@users.noreply.github.com>
2025-10-24 16:11:37 +0300

    Implement age-based birthday greeting program
    The program now asks for the user's age and prints 'Happy Birthday!' that many times.

--- ex2.c
+++ ex2.c
@@ -1,1 +1,17 @@
+//Write a program that asks for someone's age and prints "Happy Birthday!" that many times.
+// Made by Muhammed Emre Dere
+#include <stdio.h>
+int main() {
+
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    for (int i = 1; i <= age; i++) {
+        printf( "Happy Birthday \n" );
+    }
+
+}
+
 
