d0ce89ea2b73b9c64251fe071263054af894c7f8
Emre <65902526+EmreDr@users.noreply.github.com>
2025-10-24 16:37:54 +0300

    Add program to count from 2 to 10 by twos

--- ex4.c
+++ ex4.c
@@ -1,1 +1,13 @@
 
+//Write a program that counts from 2 to 10 by twos and prints each number.
+//Made by Muhammed Emre Dere.
+#include <stdio.h>
+int main() {
+    printf("Counting from 2 to 10 by twos:\n");
+    for (int i = 2; i <= 10; i += 2) {
+        printf("%d\n", i);
+    }
+
+}
+
+
